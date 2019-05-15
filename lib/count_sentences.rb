7dec025868cf1492b4b74b1e6f81e36aa6951fa2
Chet  Chopra <chopra.chet@gmail.com>
2019-05-15 22:50:13 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    return self[self.length - 1] == '.'
   end
 
   def question?
-
+    return self[self.length - 1] == '?'
   end
 
   def exclamation?
-
+    return self[self.length - 1] == '!'
   end
 
   def count_sentences
-
+    sentence_count = 0
+    self.length.times do |n|
+      if self[n] == '!' || self[n] == '?' || self[n] == '.'
+        sentence_count += 1
+      end
+    end
+    return sentence_count
   end
+  
 end
